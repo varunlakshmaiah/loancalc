9096b95c52ff5ce14336bb470339308bbf62b3bd
Varun Lakshmaiah <varunlakshmaiah@gmail.com>
2017-08-06 19:17:23 +0530

    Add Loancalc.emi function

--- loancalc.js
+++ loancalc.js
@@ -1,9 +1,39 @@
 (function(window){
     'use strict';
     function define_library(){
-        var Loancalc = {};        
+        var Loancalc = {};
+
+        var toMonth = function(Obj){
+            if(Obj.name === "month")
+          {
+            return Obj.value;
+          }
+          else if (Obj.name === "year") {
+              return Obj.value*12;
+          }
+
+        }
+
+        var decInterest = function(value){
+          return ((value/100)/12);
+        }
+
         Loancalc.test = function(){
             console.log("Loancalc Working");
+        }
+
+        Loancalc.principal = function(Obj){
+
+        }
+
+        Loancalc.emi = function(Obj){
+          console.log(Obj);
+            var p = Obj.principal;
+            var i = decInterest(Obj.roi);
+            var n = toMonth(Obj.tenure);
+            var emi = 0;
+                emi = (p*i*(Math.pow((1+i),n)))/(Math.pow((1+i),n)-1);
+           return emi.toFixed(2);
         }
 
         return Loancalc;
